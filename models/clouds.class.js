4f9d0ac8f2941cd9dfb88e3c09c8fbb2b7eb9b82
Winktory1304 <thomaswinkler88@googlemail.com>
2024-05-23 20:08:22 +0200

    animate clouds

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,11 +1,19 @@
-class Clouds extends MovableObject{
+class Clouds extends MovableObject {
     height = 250;
+    y = 20;
+    width = 250;
 
-    constructor(){
+
+    constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.y = 20;
-        this.x =Math.random() * 700;
-        this.width = 250;
-        
-    };
+        this.x = Math.random() * 700; //Zahl zwischen 0 und 700
+        debugger;
+        this.animate();
+    }
+    animate() {
+        setInterval( () => {
+            this.x -= 0.15;            
+        }, 1000 / 60);
+    }
+    
 }
