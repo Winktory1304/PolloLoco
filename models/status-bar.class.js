a086bc553f0a0e013ba8808db690e796dadcca45
Winktory1304 <thomaswinkler88@googlemail.com>
2024-06-10 17:20:29 +0200

    fixed img prob

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,11 +1,11 @@
 class StatusBar extends DrawableObject{
     IMAGES = [
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-        'PolloLoco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        './img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
     percentage = 100;
 
