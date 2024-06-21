bd207dd354d9e7a6154cf066d7ce5955668471db
Winktory1304 <thomaswinkler88@googlemail.com>
2024-06-21 16:58:26 +0200

    fixed bug with coin and bottle

--- models/statusbar-bottle.class.js
+++ models/statusbar-bottle.class.js
@@ -29,13 +29,13 @@ class StatusBarBottle extends DrawableObject{
     resolveImageIndex(){
         if(this.percentage == 5){
             return 5;
-        }else if(this.percentage >4){
+        }else if(this.percentage == 4){
             return 4;
-        }else if(this.percentage >3){
+        }else if(this.percentage == 3){
             return 3;
-        }else if(this.percentage >2){
+        }else if(this.percentage == 2){
             return 2;
-        }else if(this.percentage >1){
+        }else if(this.percentage == 1){
             return 1;
         }else{
             return 0;
