f063462d9d763aea491229d6dfb57259c01fe963
Thomas Winkler <thomaswinkler88@googlemail.com>
2024-06-24 09:19:33 +0200

    fixed chciken dead animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -16,6 +16,7 @@ class Chicken extends MovableObject{
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 500; //spawnen zufällig in einer range von 200px bis 700px
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
