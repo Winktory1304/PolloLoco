ac35f1a0f395618d03fd0be2d699dbcdf6284b14
Winktory1304 <thomaswinkler88@googlemail.com>
2024-06-26 16:36:46 +0200

    endboss first contact

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,10 +1,17 @@
-class Endboss extends MovableObject{
-
+class Endboss extends MovableObject {
+    firstContactBoss = false;
     height = 500;
     width = 300;
     y = -35;
 
-    IMAGES_WALKING =[
+    IMAGES_WALKING = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+
+    ];
+    IMAGES_SPAWNING = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
         'img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -13,18 +20,29 @@ class Endboss extends MovableObject{
         'img/4_enemie_boss_chicken/2_alert/G10.png',
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png'
-        
     ];
-    constructor(){
+    constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_SPAWNING);
         this.x = 2500;
         this.animate()
     }
 
-    animate(){
+    animate() {
+        let i = 0
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+
+            if (i < 8) {
+                this.playAnimation(this.IMAGES_SPAWNING);
+            } else {                
+                this.playAnimation(this.IMAGES_WALKING);
+            }
+            i++;
+            if (world.character.x > 1860 && !this.firstContactBoss) {
+                i = 0;
+                this.firstContactBoss = true;
+            }
         }, 200);
     }
 }
