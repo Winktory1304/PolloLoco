a8d2a210b3195382b8ccf483fb15928515164819
Winktory1304 <thomaswinkler88@googlemail.com>
2024-07-03 18:51:37 +0200

    clean code endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,12 +1,12 @@
 class Endboss extends MovableObject {    
-    IMAGE_SPAWN = '../img/4_enemie_boss_chicken/1_walk/G2.png';
-    IMAGES_WALKING = [
+    static IMAGE_SPAWN = '../img/4_enemie_boss_chicken/1_walk/G2.png';
+    static IMAGES_WALKING = [
         '../img/4_enemie_boss_chicken/1_walk/G1.png',
         '../img/4_enemie_boss_chicken/1_walk/G2.png',
         '../img/4_enemie_boss_chicken/1_walk/G3.png',
         '../img/4_enemie_boss_chicken/1_walk/G4.png'
     ];
-    IMAGES_ALERT = [
+    static IMAGES_ALERT = [
         '../img/4_enemie_boss_chicken/2_alert/G5.png',
         '../img/4_enemie_boss_chicken/2_alert/G6.png',
         '../img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -16,7 +16,7 @@ class Endboss extends MovableObject {
         '../img/4_enemie_boss_chicken/2_alert/G11.png',
         '../img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
-    IMAGES_ATTACK = [
+    static IMAGES_ATTACK = [
         '../img/4_enemie_boss_chicken/3_attack/G13.png',
         '../img/4_enemie_boss_chicken/3_attack/G14.png',
         '../img/4_enemie_boss_chicken/3_attack/G15.png',
@@ -26,76 +26,96 @@ class Endboss extends MovableObject {
         '../img/4_enemie_boss_chicken/3_attack/G19.png',
         '../img/4_enemie_boss_chicken/3_attack/G20.png'
     ];
-    IMAGES_HURT = [
+    static IMAGES_HURT = [
         '../img/4_enemie_boss_chicken/4_hurt/G21.png',
         '../img/4_enemie_boss_chicken/4_hurt/G22.png',
         '../img/4_enemie_boss_chicken/4_hurt/G23.png'
     ];
-    IMAGES_DEAD = [
+    static IMAGES_DEAD = [
         '../img/4_enemie_boss_chicken/5_dead/G24.png',
         '../img/4_enemie_boss_chicken/5_dead/G25.png',
         '../img/4_enemie_boss_chicken/5_dead/G26.png'
     ];
-    // offset = {
-    //     left: +60,
-    //     right: -110,
-    //     top: -80,
-    //     bottom: +75
-    // };
+
     width = 400;
     height = 400;
     y = 50;
     energy = 80;
     alertAnimationPlayed = false;
-
+    speed = 15;
+    x = 1750;
 
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[1]);
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_ALERT);
-        this.loadImages(this.IMAGES_ATTACK);
-        this.loadImages(this.IMAGES_HURT);
-        this.loadImages(this.IMAGES_DEAD);
-        this.x = 1750;
+        super();
+        this.initImages();
+        this.loadImage(Endboss.IMAGES_WALKING[1]);
         this.animate();
-        this.speed = 15;
     }
 
+    initImages() {
+        this.loadImages(Endboss.IMAGES_WALKING);
+        this.loadImages(Endboss.IMAGES_ALERT);
+        this.loadImages(Endboss.IMAGES_ATTACK);
+        this.loadImages(Endboss.IMAGES_HURT);
+        this.loadImages(Endboss.IMAGES_DEAD);
+    }
 
     animate() {
-        setInterval(() => {
-            if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
-                gameWon();
-                setInterval(() => {
-                    this.showImage(this.IMAGES_DEAD[2]);
-                }, this.IMAGES_DEAD.length * 150);
-                setInterval(() => {
-                    gameDone();
-                }, this.IMAGES_DEAD.length * 200);
-            } else if (this.isHurt()) {
-                this.playAnimation(this.IMAGES_HURT);
-            } else if (this.checkDistance() < 50) {
-                this.playAnimation(this.IMAGES_ATTACK);
-            } else if (this.checkDistance() < 300 && !this.alertAnimationPlayed) {
-                this.playAnimation(this.IMAGES_ALERT);
-                setInterval(() => {
-                    this.alertAnimationPlayed = true;
-                }, this.IMAGES_ALERT.length * 150);
-            } else if (this.alertAnimationPlayed) {
-                this.playAnimation(this.IMAGES_WALKING);
-                this.moveLeft();
-            } else if (this.checkDistance() > 200) {
-                this.showImage(this.IMAGE_SPAWN);
-            }
-        }, 150);
+        setInterval(() => this.handleAnimation(), 150);
     }
 
+    handleAnimation() {
+        if (this.isDead()) {
+            this.handleDeath();
+        } else if (this.isHurt()) {
+            this.playAnimation(Endboss.IMAGES_HURT);
+        } else if (this.isInAttackRange()) {
+            this.playAnimation(Endboss.IMAGES_ATTACK);
+        } else if (this.isInAlertRange() && !this.alertAnimationPlayed) {
+            this.handleAlert();
+        } else if (this.alertAnimationPlayed) {
+            this.walkAndMoveLeft();
+        } else {
+            this.showSpawnImage();
+        }
+    }
+
+    handleDeath() {
+        this.playAnimation(Endboss.IMAGES_DEAD);
+        gameWon();
+        setTimeout(() => this.showImage(Endboss.IMAGES_DEAD[2]), Endboss.IMAGES_DEAD.length * 150);
+        setTimeout(() => gameDone(), Endboss.IMAGES_DEAD.length * 200);
+    }
+
+    isInAttackRange() {
+        return this.checkDistance() < 50;
+    }
+
+    isInAlertRange() {
+        return this.checkDistance() < 300;
+    }
+
+    handleAlert() {
+        this.playAnimation(Endboss.IMAGES_ALERT);
+        setTimeout(() => {
+            this.alertAnimationPlayed = true;
+        }, Endboss.IMAGES_ALERT.length * 150);
+    }
+
+    walkAndMoveLeft() {
+        this.playAnimation(Endboss.IMAGES_WALKING);
+        this.moveLeft();
+    }
+
+    showSpawnImage() {
+        if (this.checkDistance() > 200) {
+            this.showImage(Endboss.IMAGE_SPAWN);
+        }
+    }
 
     checkDistance() {
         return this.x - world.character.x;
     }
-
 
     showImage(image) {
         this.img = this.imageCache[image];
