368e5b24ce72c7876d957a820297e134aac9e880
Winktory1304 <thomaswinkler88@googlemail.com>
2024-07-27 13:30:13 +0200

    add controls  modal

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 let canvas;
-let world; 
+let world;
 let keyboard = new Keyboard();
 let winSound = new Audio('audio/winSound.mp3');
 let defeatSound = new Audio('audio/defeatSound.mp3');
@@ -7,7 +7,7 @@ let gameLost = false;
 let gameWon = false;
 
 
-function startGame(){
+function startGame() {
     gameLost = false;
     gameWon = false;
     init();
@@ -15,7 +15,7 @@ function startGame(){
 
 
 
-function endTheGameByLost(){
+function endTheGameByLost() {
     if (gameLost) return; // Wenn das Spiel bereits verloren wurde, nichts tun
     world.endboss.bossMusic.pause();
     gameLost = true; // Setzt den Status auf verloren    
@@ -31,10 +31,10 @@ function endTheGameByLost(){
     gameScreen.style.display = 'none';
 }
 
-function endTheGameByWin(){
+function endTheGameByWin() {
     if (gameWon) return;
     world.endboss.bossMusic.pause();
-    gameWon = true; 
+    gameWon = true;
     winSound.play();
     let startScreen = document.getElementById('startScreen');
     startScreen.style.display = 'none';
@@ -46,16 +46,16 @@ function endTheGameByWin(){
     gameScreen.style.display = 'none';
 }
 
-function init(){
+function init() {
     startGame();
     initLevel1();
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
     movableObject = new MovableObject();
-    console.log('My character is', world.character);   
+    console.log('My character is', world.character);
 }
 
-function startGame(){
+function startGame() {
     let startScreen = document.getElementById('startScreen');
     startScreen.style.display = 'none';
     let defeatScreen = document.getElementById('defeatScreen');
@@ -66,10 +66,10 @@ function startGame(){
     gameScreen.style.display = 'unset';
     let controlContainer = document.getElementById('controlsContainer');
     controlContainer.style.display = 'none';
-    
+
 }
 
-function startScreen(){
+function startScreen() {
     let startScreen = document.getElementById('startScreen');
     startScreen.style.display = 'unset';
     let defeatScreen = document.getElementById('defeatScreen');
@@ -81,44 +81,69 @@ function startScreen(){
 
 }
 
-window.addEventListener('keydown', (e) =>{
-    if(e.code == "ArrowRight"){        
+window.addEventListener('keydown', (e) => {
+    if (e.code == "ArrowRight") {
         keyboard.RIGHT = true;
     }
-    if(e.code == "ArrowLeft"){
+    if (e.code == "ArrowLeft") {
         keyboard.LEFT = true;
     }
-    if(e.code == "ArrowDown"){
+    if (e.code == "ArrowDown") {
         keyboard.DOWN = true;
     }
-    if(e.code == "ArrowUp"){
+    if (e.code == "ArrowUp") {
         keyboard.UP = true;
     }
-    if(e.code == "Space"){
+    if (e.code == "Space") {
         keyboard.SPACE = true;
     }
-    if(e.keyCode == 68){
-        keyboard.D = true;        
+    if (e.keyCode == 68) {
+        keyboard.D = true;
     }
 });
 
-window.addEventListener('keyup', (e) =>{
-    if(e.code == "ArrowRight"){ 
+window.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowRight") {
         keyboard.RIGHT = false;
     }
-    if(e.code == "ArrowLeft"){
+    if (e.code == "ArrowLeft") {
         keyboard.LEFT = false;
     }
-    if(e.code == "ArrowDown"){
+    if (e.code == "ArrowDown") {
         keyboard.DOWN = false;
     }
-    if(e.code == "ArrowUp"){
+    if (e.code == "ArrowUp") {
         keyboard.UP = false;
     }
-    if(e.code == "Space"){
+    if (e.code == "Space") {
         keyboard.SPACE = false;
     }
-    if(e.keyCode == 68){
-        keyboard.D = false;        
+    if (e.keyCode == 68) {
+        keyboard.D = false;
     }
 });
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
