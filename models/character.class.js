474b47d620ee4d2a19a9b8130ad7b8f5a1b97aec
Winktory1304 <thomaswinkler88@googlemail.com>
2024-07-28 13:36:30 +0200

    snore timer

--- models/character.class.js
+++ models/character.class.js
@@ -148,7 +148,7 @@ class Character extends MovableObject {
 
     checkIdle() {
         this.idleTimer = null;
-        this.idleTime = 555000; // 5 seconds
+        this.idleTime = 3000; // 5 seconds
 
         this.startIdleTimer();
     }
