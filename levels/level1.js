744c76503fe18fab39a658162a8ccb341942b942
Winktory1304 <thomaswinkler88@googlemail.com>
2024-07-29 17:13:31 +0200

    more  coins and bottles

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
-let level1; 
-function initLevel1(){
+let level1;
+function initLevel1() {
 
     level1 = new Level(
         [
@@ -15,7 +15,7 @@ function initLevel1(){
             new SmallChicken(),
             new SmallChicken(),
             // new Endboss()
-        ],    
+        ],
         [
             new Clouds()
         ],
@@ -24,45 +24,51 @@ function initLevel1(){
             new Coins(250, 325),
             new Coins(350, 300),
             new Coins(500, 100),
-            new Coins(),
-            new Coins(),
-            new Coins(),
-            new Coins(),
-            new Coins(),
-            new Coins()
+            new Coins(650, 200),
+            new Coins(800, 150),
+            new Coins(950, 250),
+            new Coins(1100, 350),
+            new Coins(1250, 200),
+            new Coins(1300, 300)
+            
         ],
         [
-            new Bottles(250,120),
-            new Bottles(400,120),
-            new Bottles(700,120),
-            new Bottles(1000,120),
-            new Bottles(1250,120),
-            
+            new Bottles(250, 120),
+            new Bottles(400, 120),
+            new Bottles(700, 120),
+            new Bottles(1000, 120),
+            new Bottles(1250, 120),
+            new Bottles(300, 220),
+            new Bottles(450, 220),
+            new Bottles(750, 220),
+            new Bottles(1050, 220),
+            new Bottles(1300, 220)
+
         ],
         [
             new BackgroundObject("img/5_background/layers/air.png", -719),
             new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
             new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
             new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-    
+
             new BackgroundObject("img/5_background/layers/air.png", 0),
             new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
             new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),        
+            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
             new BackgroundObject("img/5_background/layers/air.png", 719),
             new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
             new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
             new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
-    
-            new BackgroundObject("img/5_background/layers/air.png", 719*2),
-            new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719*2),
-            new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719*2),
-            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),        
-            new BackgroundObject("img/5_background/layers/air.png", 719*3),
-            new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719*3),
-            new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719*3),
-            new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
-            
+
+            new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
+
         ]
     )
 }
